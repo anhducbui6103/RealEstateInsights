85ee461935bfaf66fadcf9cf08bee4147cdf54a7
anhducbui6103 <anhduc.bui.6103@gmail.com>
2024-03-27 14:22:54 +0700

    fig: stop at element 6 by handling NoSuchElementExeption

--- src/DataCollector.java
+++ src/DataCollector.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonParser;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -23,61 +24,73 @@ public class DataCollector {
         options.addArguments("--force-device-scale-factor=1");
         WebDriver driver = new EdgeDriver(options);
 
-        try{
+        try {
             // Open Web
             driver.get("https://www.nhatot.com/mua-ban-bat-dong-san-ha-noi");
             Thread.sleep(5000);
 
             // Data Collect
             JSONArray jsonArray = new JSONArray();
-            List<WebElement> collectionElements = driver.findElements(By.xpath("//*[@id=\"__next\"]/div/div[4]/div[1]/div[2]/main/div[1]/div[4]/div/div[1]/ul/div[*]"));
+            int elementCount = 0;
+            while (elementCount < 100) {
+                List<WebElement> collectionElements = driver.findElements(By.xpath("//*[@id=\"__next\"]/div/div[4]/div[1]/div[2]/main/div[1]/div[4]/div/div[1]/ul/div[*]"));
 
-            for (WebElement element : collectionElements) {
-                String title = element.findElement(By.xpath("./li/a/div[2]/div/h3")).getText();
-                String info = element.findElement(By.xpath("./li/a/div[2]/div/div[1]")).getText();
-                String price = element.findElement(By.xpath("./li/a/div[2]/div/div[2]/div/p")).getText();
+                for (WebElement element : collectionElements) {
+                    WebElement titleElement = null;
+                    try {
+                        titleElement = element.findElement(By.xpath("./li/a/div[2]/div/h3"));
+                    } catch (NoSuchElementException e) {
+                        continue;
+                    }
 
-                String[] infoParts = info.split(" - ");
-                String area = "";
-                String rooms = "";
+                    String title = element.findElement(By.xpath("./li/a/div[2]/div/h3")).getText();
+                    String info = element.findElement(By.xpath("./li/a/div[2]/div/div[1]")).getText();
+                    String price = element.findElement(By.xpath("./li/a/div[2]/div/div[2]/div/p")).getText();
 
-                if (infoParts.length >= 2) {
-                    area = infoParts[0].trim();
-                    rooms = infoParts[1].trim();
+                    String[] infoParts = info.split(" - ");
+                    String area = "";
+                    String rooms = "";
+
+                    if (infoParts.length >= 2) {
+                        area = infoParts[0].trim();
+                        rooms = infoParts[1].trim();
+                    }
+
+                    String[] priceParts = price.split(" \\| ");
+                    String cost = "";
+                    String costPerM2 = "";
+
+                    if (priceParts.length >= 2) {
+                        cost = priceParts[0].trim();
+                        costPerM2 = priceParts[1].trim();
+                    }
+
+                    // Create JSON Object
+                    JSONObject nhaDatData = new JSONObject();
+                    nhaDatData.put("Title", title);
+                    nhaDatData.put("Area", area);
+                    nhaDatData.put("Rooms", rooms);
+                    nhaDatData.put("Total Cost", cost);
+                    nhaDatData.put("Cost Per M2", costPerM2);
+
+                    jsonArray.add(nhaDatData);
+                    elementCount++;
                 }
-
-                String[] priceParts = price.split(" \\| ");
-                String cost = "";
-                String costPerM2 = "";
-
-                if (priceParts.length >= 2) {
-                    cost = priceParts[0].trim();
-                    costPerM2 = priceParts[1].trim();
-                }
-
-                // Create JSON Object
-                JSONObject nhaDatData = new JSONObject();
-                nhaDatData.put("Title", title);
-                nhaDatData.put("Area", area);
-                nhaDatData.put("Rooms", rooms);
-                nhaDatData.put("Total Cost", cost);
-                nhaDatData.put("Cost Per M2", costPerM2);
-
-                jsonArray.add(nhaDatData);
-
-                // Export JSON data
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                String formattedJson = gson.toJson(JsonParser.parseString(jsonArray.toString()));
-                try (FileWriter fileWriter = new FileWriter("datastorage/data.json")) {
-                    fileWriter.write(formattedJson);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
+//
+//                WebElement newPage = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[4]/div[1]/div[2]/main/div[1]/div[5]/div/div/div[10]/button"));
+//                newPage.click();
+            }
+            // Export JSON data
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String formattedJson = gson.toJson(JsonParser.parseString(jsonArray.toString()));
+            try (FileWriter fileWriter = new FileWriter("datastorage/data.json")) {
+                fileWriter.write(formattedJson);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             driver.quit();
         }
     }
